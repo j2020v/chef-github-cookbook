0487c881d197b4e96ad939e7e97eabd33a2f06f2
Jillian Guillen <JGuillen@spartaglobal.com>
2019-10-31 20:56:13 +0000

    added tests for default spec

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,7 +7,7 @@
 require 'spec_helper'
 
 describe 'github::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
     platform 'ubuntu', '18.04'
@@ -17,13 +17,24 @@ describe 'github::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install python from package' do
+      expect(chef_run).to install_package 'python'
     end
+
+    it 'should install requests plugin using pip management' do
+      expect(chef_run).to run execute ('install requests==2.3.0')
+    end
+
+    it 'should install nginx' do
+      expect(chef_run).to install_package 'nginx'
+    end
+
+    it 'enables the nginx service' do
+      expect(chef_run).to start_service 'nginx'
+    end
+
+    it 'starts the nginx service' do
+      expect(chef_run).to start_service 'nginx'
+    end
+
   end
-end
