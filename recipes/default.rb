3355edc0af7cf4e618c1f17b00f9e1e012530b76
Jillian Guillen <JGuillen@spartaglobal.com>
2019-10-31 20:56:48 +0000

    including recipe python and node

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,9 @@
 #
-# Cookbook:: github
-# Recipe:: default
+Cookbook:: github
+Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 
-install 'policyfile'
-
-include_recipe 'python_recipe'
+include_recipe 'python'
+include_recipe 'node'
